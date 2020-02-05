7ed887b3506d0e89fdbe2b0e908ea1ecdfe2692b
Nancy Zhu <nancyzhu@MacBook-Air-3.local>
2020-02-04 21:53:59 -0800

    First Commit

---
+++ practice.R
@@ -0,0 +1,39 @@
+## Practice the following problem 
+
+# What is the sum of the first 1000 positive integers?
+
+#1. Use the function c to create a vector with the average high temperatures in January for
+#Beijing, Lagos, Paris, Rio de Janeiro, San Juan, and Toronto, which are 35, 88, 42, 84, 81, 
+#and 30 degrees Fahrenheit. Call the object temp.
+
+# Now create a vector with the city names and call the object city.
+
+# Use the names function and the objects defined in the previous exercises to associate the temperature data with its corresponding city.
+
+# Use the [ and : operators to access the temperature of the first three cities on the list.
+             
+# Use the [ operator to access the temperature of Paris and San Juan.
+                          
+# Use the : operator to create a sequence of numbers  12,13,14,.....73
+                        
+                          
+# Create a vector containing all the positive odd numbers smaller than 100.
+                          
+# Create a vector of numbers that starts at 6, does not pass 55, and adds numbers in increments of 4/7: 6, 6 + 4/7, 6 + 8/7, and so on. How many numbers does the list have? Hint: use seq and length.
+                          
+# What is the class of the following object a <- seq(1, 10, 0.5)?
+                            
+#What is the class of the following object a <- seq(1, 10)?
+                            
+# The class of class(a<-1) is numeric, not integer. R defaults to numeric and to force an integer, you need to add the letter L. Confirm that the class of 1L is integer.
+                          
+# Define the following vector:x <- c("1", "3", "5") and coerce it to get integers.
+
+
+
+#  In the data frame you made in the second problem convert the temperature from Fahrenheit to Celsius. The conversion is  
+#C=5/9×( F −32)
+
+
+
+
